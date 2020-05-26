f54c3de8c679bab834b5bdd5664d683c68968693
HumamShwaikh <hshwa039@uottawa.ca>
2020-05-25 22:01:48 -0600

    generate json

---
+++ Disinformation-Challenge/generate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import csv
+import json
+import collections
+
+# aliases
+OrderedDict = collections.OrderedDict
+
+src = 'sheet1.csv'
+dst = 'data.json'
+header = [
+    'Source','Article' 'Title' ,'URL','Partisanship Rating','Credibility Rating','Article Text'
+]
+
+data = []
+with open(src, 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        if row[0].strip()[0] == '#':  #
+            continue
+        row = filter(None, row)
+        data.append(OrderedDict(zip(header, row)))
+
+with open(dst, 'w') as jsonfile:
+    json.dump(data, jsonfile, indent=2)
