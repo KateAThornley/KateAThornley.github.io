c858a218b01f68a59ea1f9ab0ba0062d0a0176aa
KateAThornley <ktott56789@gmail.com>
2020-05-26 00:02:51 -0400

    uhghhh

--- Disinformation-Challenge/scripts/main.js
+++ Disinformation-Challenge/scripts/main.js
@@ -1,45 +1,55 @@
-var ctx = document.getElementById('myChart');
+var ctx = document.getElementById("myChart");
 
 ctx.onclick = function (evt) {
-	var activePoints = myChart.getElementsAtEvent(evt);
-	console.log(activePoints[0]._index);
+  var activePoints = myChart.getElementsAtEvent(evt);
+  console.log(activePoints[0]._index);
+  var contentTitle = myChart[activePoints[0]._index - 1].label;
+  console.log(contentTitle);
 };
 
 var myChart = new Chart(ctx, {
-	type: 'scatter',
-	data: {
-		datasets: [{
-			label: ['Scatter Dataset'],
-			data: [{
-				// Label for the tooltip
-				label: 'test',
-				// Value for the tooltip
-				value: 'string',
-				x: 5,
-				y: 0
-			}, {
-				// Label for the tooltip
-				label: 'test',
-				// Value for the tooltip
-				value: 'string',
-				x: 0,
-				y: 10
-			}, {
-				// Label for the tooltip
-				label: 'test',
-				// Value for the tooltip
-				value: 'string',
-				x: 10,
-				y: 5
-			}]
-		}]
-	},
-	options: {
-		scales: {
-			xAxes: [{
-				type: 'linear',
-				position: 'bottom'
-			}]
-		}
-	}
+  type: "scatter",
+  data: {
+    datasets: [
+      {
+        label: ["Scatter Dataset"],
+        data: [
+          {
+            // Label for the tooltip
+            label: "test",
+            // Value for the tooltip
+            value: "string",
+            x: 5,
+            y: 0,
+          },
+          {
+            // Label for the tooltip
+            label: "test",
+            // Value for the tooltip
+            value: "string",
+            x: 0,
+            y: 10,
+          },
+          {
+            // Label for the tooltip
+            label: "test",
+            // Value for the tooltip
+            value: "string",
+            x: 10,
+            y: 5,
+          },
+        ],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      xAxes: [
+        {
+          type: "linear",
+          position: "bottom",
+        },
+      ],
+    },
+  },
 });
